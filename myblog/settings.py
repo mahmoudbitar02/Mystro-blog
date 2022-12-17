d499065608034172ddaa0b2b30c823f1a814b719
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-12-17 20:40:02 +0100

    add image & tags

--- myblog/settings.py
+++ myblog/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "taggit",
+
     "posts",
 ]
 
@@ -119,7 +122,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
 class Posts(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=10000)
+    image = models.ImageField(upload_to='posts/')
+    tags = TaggableManager()
     
     def __str__(self):
         return self.title
