42dca1d2d3b78d762824b58ee215f415a61285fb
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-12-26 17:16:04 +0100

    add site

--- myblog/urls.py
+++ myblog/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from posts.views import post_list, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', post_list),
+    path('blog/<int:id>', post_detail)
 ]
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from.models import Posts
 
 # Create your views here.
+
+def post_list(request):
+    all = Posts.objects.all()
+    return render (request, 'posts.html', {'data':all})
+
+
+def post_detail(request,id):
+    post = Posts.objects.get(id=id)
+    return render(request, 'single.html', {'data':post})
